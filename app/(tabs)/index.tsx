00450add1354c3d9a4506da1439cd67a5dff51d5
Prince Dariouz Jon G. Baybay <pgbaybay.student@asiancollege.edu.ph>
2025-12-14 09:53:32 +0800

    GHEOAEK

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,11 +18,11 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome to world!</ThemedText>
+        <ThemedText type="title">Welcome to world fck!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Step 1: Try it fck it</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
@@ -39,7 +39,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+            <ThemedText type="subtitle">Step 2: Explor heeloo</ThemedText>
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
